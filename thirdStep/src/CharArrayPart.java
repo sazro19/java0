5a36abf6239d74aa1d20da581e0234043fe4fd0e
Alexander <sasha.zalesskiy@mail.ru>
2021-02-20 13:48:16 +0300

    4 tasks from Strings

--- thirdStep/src/CharArrayPart.java
+++ thirdStep/src/CharArrayPart.java
@@ -5,7 +5,11 @@ public class CharArrayPart {
     public static void main(String[] args) {
         toSnakeCase();
         line();
-        String s = new String();
+        changeToLetter("abcdwordkoshpwordSfagry");
+        line();
+        figuresAmount("abcdqwrzxfg015 43asfsdf2a0");
+        line();
+        numbersAmount("abdc01023s 15d_+- d34s");
     }
 
     public static void toSnakeCase() {
@@ -24,6 +28,63 @@ public class CharArrayPart {
             snake[i] = temp;
         }
         System.out.println(Arrays.toString(snake));
+    }
+
+    public static void changeToLetter(String s) {
+        String temp = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'w' && i <= s.length() - 4) {
+                if (s.charAt(i + 1) == 'o') {
+                    if (s.charAt(i + 2) == 'r') {
+                        if (s.charAt(i + 3) == 'd') {
+                            temp += "letter";
+                            if (i + 4 < s.length()) {
+                                i += 4;
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            temp += s.charAt(i);
+        }
+        s = temp;
+        System.out.println(s);
+    }
+
+    public static void figuresAmount(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (isFigure(s.charAt(i))) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    private static boolean isFigure(char c) {
+        return (int) c >= 48 && (int) c <= 57;
+    }
+
+    public static void numbersAmount(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (isFigure(s.charAt(i))) {
+                if (s.charAt(i) == '0'){
+                    count++;
+                    continue;
+                }
+                for (int j = i; j < s.length(); j++) {
+                    if (!isFigure(s.charAt(j)) || j == s.length() - 1) {
+                        count++;
+                        i = j;
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println(count);
     }
 
     public static void line() {
